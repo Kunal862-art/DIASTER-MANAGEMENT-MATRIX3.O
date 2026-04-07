03e32e438caec37a7c19946ba7c047a25caaabd4
kunal862-art <kunalbansal2006@gmail.com>
2026-04-07 10:36:10 +0530

    Security: Purged api_config.py from git cache to stop leaks

--- api_config.py
+++
@@ -1,8 +0,0 @@
-# WARNING: Never share this file or upload it to public GitHub repositories!
-# 
-# 1. Go to https://aistudio.google.com/app/apikey
-# 2. Click "Create API Key"
-# 3. Copy the brand new key
-# 4. Paste it strictly between the quotation marks below replacing 'PASTE_NEW_KEY_HERE':
-
-GEMINI_API_KEY = "AIzaSyC0FMVFeFmhyrdjt9gpSdFgGH5U61KDnzU"
